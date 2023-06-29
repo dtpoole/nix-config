f6e5237596d62ccc99320701e87358dfd84bd095
David Poole <dtpoole@users.noreply.github.com>
2023-06-29 17:11:50 -0400

    add treesitter

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -18,6 +18,29 @@
       fzf-vim
       tcomment_vim
       vim-fugitive
+
+      {
+        plugin = nvim-treesitter.withPlugins (p: [ 
+          p.bash p.dockerfile
+          p.git_config p.git_rebase p.gitattributes p.gitcommit p.gitignore 
+          p.markdown p.sql p.yaml p.json p.make p.nix p.python p.terraform p.vim
+        ]);
+        
+        type = "lua";
+        config = ''
+          require'nvim-treesitter.configs'.setup {
+            highlight = {
+              enable = true,
+              disable = {},
+            },
+            indent = {
+              enable = false,
+              disable = {},
+            },
+          }
+        '';
+      }
+
     ];
 
     extraConfig = builtins.readFile ./init.vim;
