146ead271db3a875fc5cf86a85b6c75142969fea
David Poole <dtpoole@users.noreply.github.com>
2023-07-07 17:25:16 -0400

    add extraSpecialArgs to hm config

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.${username} = import ./home;
+              home-manager.extraSpecialArgs = { inherit inputs outputs username; };
             }
           ];
         };
