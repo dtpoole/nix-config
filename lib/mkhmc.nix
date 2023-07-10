47085c272ad6c1c0e07378f998f81313cd76e944
David Poole <dtpoole@users.noreply.github.com>
2023-07-10 16:52:25 -0400

    refactor

--- lib/mkhmc.nix
+++
@@ -1,8 +0,0 @@
-# This function creates a nix-darwin system.
-name: { nixpkgs, home-manager, system, username }:
-
-system: home-manager.lib.homeManagerConfiguration rec {
-    extraSpecialArgs = { inherit inputs outputs username; };
-    modules = [ ./home ];
-    pkgs = nixpkgs.legacyPackages."${system}";
-};
