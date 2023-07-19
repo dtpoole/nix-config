7aa3543fdb64938619e63603e8a8002b8e4e6280
David Poole <dtpoole@users.noreply.github.com>
2023-07-19 16:44:01 -0400

    remove disk swap

--- hosts/slug/hardware-configuration.nix
+++ hosts/slug/hardware-configuration.nix
@@ -26,8 +26,8 @@
       fsType = "vfat";
     };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/19a8e837-8b74-4489-a756-09b0cfd5467b"; }];
+  # swapDevices =
+  #   [{ device = "/dev/disk/by-uuid/19a8e837-8b74-4489-a756-09b0cfd5467b"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
