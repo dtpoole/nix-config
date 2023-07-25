09d565d13d9ef8d8de140909141255258832f1f7
David Poole <dtpoole@users.noreply.github.com>
2023-07-25 16:26:30 -0400

    Add missing hasGUI

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.${username} = import ./home;
-            home-manager.extraSpecialArgs = { inherit username; };
+            home-manager.extraSpecialArgs = { inherit username hasGUI; };
           }
         ];
       };
