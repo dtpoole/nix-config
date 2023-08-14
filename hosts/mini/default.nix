077a0bd3a98ffadb16a2fba4cad6a0c71454760a
David Poole <dtpoole@users.noreply.github.com>
2023-08-14 16:59:46 -0400

    add nix config

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -1,10 +1,21 @@
 { pkgs, ... }:
 {
 
+
+  nix = {
+    #package = lib.mkDefault pkgs.unstable.nix;
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        warn-dirty = false;
+      };
+    };
+ # services.nix-daemon.enable = true;
+
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
+
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
+  #services.nix-daemon.package = pkgs.nixFlakes;
 
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
