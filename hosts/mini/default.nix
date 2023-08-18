3901588cde02d0f19d193cd19e45fb9bfe13d933
David Poole <dtpoole@users.noreply.github.com>
2023-08-18 15:12:57 -0400

    specify system and username

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
+{ system, username, ... }:
 {
+
+  nixpkgs.hostPlatform = system;
+
+  users.users.${username}.home = "/Users/${username}";
 
   nix = {
     #package = lib.mkDefault pkgs.unstable.nix;
@@ -11,16 +15,14 @@
 
   nixpkgs.config.allowUnfree = true;
 
-
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
 
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
+  #services.nix-daemon.package = pkgs.nixFlakes;
 
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
   programs.zsh.enable = true;
-  # bash is enabled by default
 
 }
