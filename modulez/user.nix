9d5d18c0b6b3c03f5b220d9ac2c1d1c1643928ae
David Poole <dtpoole@users.noreply.github.com>
2023-08-25 16:57:27 -0400

    Add docker group

--- modulez/user.nix
+++ modulez/user.nix
@@ -7,7 +7,7 @@
   users.users.${username} = {
     isNormalUser = true;
     home = "/home/${username}";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "docker" "wheel" "networkmanager" ];
     shell = pkgs.zsh;
     hashedPassword = "$y$j9T$gmxeDrPZuC4w8BUb.sy0Y0$I79zQJQbz9v9PF29tmq5mKe1m.At9Lvgtnh5D2VkZk1";
   };
