1d5fcf086161304b1e675e77940d27fcd4eeceb9
David Poole <dtpoole@users.noreply.github.com>
2023-08-31 16:30:43 -0400

    Add tailscale.nix

---
+++ modulez/tailscale.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  services.tailscale.enable = true;
+  
+  # Tell the firewall to implicitly trust packets routed over Tailscale:
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+}
