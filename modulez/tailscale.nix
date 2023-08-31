bb13eb567d5575ace28deecf5c984803558308b4
David Poole <dtpoole@users.noreply.github.com>
2023-08-31 16:38:14 -0400

    Use pkg from unstable

--- modulez/tailscale.nix
+++ modulez/tailscale.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ unstablePkgs, ... }:
 
 {
   services.tailscale.enable = true;
+  services.tailscale.package = unstablePkgs.tailscale;
   
   # Tell the firewall to implicitly trust packets routed over Tailscale:
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
