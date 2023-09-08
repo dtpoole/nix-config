c24c6f07f868ac7b282339512eeef75833e322d9
David Poole <dtpoole@users.noreply.github.com>
2023-09-08 14:11:23 -0400

    cleanup

--- hosts/crunch/acme.nix
+++ hosts/crunch/acme.nix
@@ -1,31 +1,25 @@
 { config, pkgs, ... }:
 
-{  
-  
+{
+
   age.secrets.acme_credentials.file = ../../secrets/acme_cloudflare_credentials.age;
 
   security.acme = {
     defaults = {
       email = "acme@poole.foo";
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck = false;
+      credentialsFile = "${config.age.secrets.acme_credentials.path}";
     };
     acceptTerms = true;
     certs = {
       "crunch.poole.foo" = {
-        domain = "crunch.poole.foo";
-        dnsProvider = "cloudflare";
-        dnsResolver = "1.1.1.1:53";
-        credentialsFile = "${config.age.secrets.acme_credentials.path}";
         extraDomainNames = [ "*.crunch.poole.foo" ];
-        dnsPropagationCheck = false;
       };
-      "git.poole.foo" = {
-        domain = "git.poole.foo";
-        dnsProvider = "cloudflare";
-        dnsResolver = "1.1.1.1:53";
-        credentialsFile = "${config.age.secrets.acme_credentials.path}";
-        dnsPropagationCheck = false;
-      };
- 
+      "git.poole.foo" = { };
+      "links.poole.foo" = { };
+
     };
   };
 
