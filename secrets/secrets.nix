6086a2f637b199c0855e5f07c9f3b9ac8545f72b
David Poole <dtpoole@users.noreply.github.com>
2023-09-08 15:54:56 -0400

    Add restic secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,6 +5,7 @@ let
   };
   users = {
     dave_mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ dave@mini.local";
+    dave_air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8HIreGLHjE8f2kpfd49WRCfXXk2oMRApuIW78BWYVi";
   };
   allUsers = builtins.attrValues users;
   allSystems = builtins.attrValues systems;
@@ -13,4 +14,6 @@ in
   "test.age".publicKeys = allUsers ++ [ systems.mini ];
   "crunch_hc_ping_uuid.age".publicKeys = allUsers ++ [ systems.crunch ];
   "acme_cloudflare_credentials.age".publicKeys = allUsers ++ [ systems.crunch ];
+  "restic_repository.age".publicKeys = allUsers ++ allSystems;
+  "restic_repository_password.age".publicKeys = allUsers ++ allSystems;
 }
