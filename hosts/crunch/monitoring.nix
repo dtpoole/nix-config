5f480dd0a084e2f66d2caeed79e9da435579b460
David Poole <dtpoole@users.noreply.github.com>
2023-09-18 15:18:59 -0400

    make work

--- hosts/crunch/monitoring.nix
+++ hosts/crunch/monitoring.nix
@@ -1,18 +1,66 @@
 { config, pkgs, ... }: {
 
-  services.grafana = {
+  services.prometheus = {
+    port = 3020;
     enable = true;
-    domain = "grafana.crunch.poole.foo";
-    port = 2342;
-    addr = "127.0.0.1";
+
+    exporters = {
+      node = {
+        port = 3021;
+        enabledCollectors = [ "systemd" ];
+        enable = true;
+      };
+    };
+
+    # ingest the published nodes
+    scrapeConfigs = [{
+      job_name = "nodes";
+      static_configs = [{
+        targets = [
+          "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
+        ];
+      }];
+    }];
   };
 
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+  services.grafana = {
+    enable = true;
+
+    settings = {
+      server = {
+        domain = "grafana.crunch.poole.foo";
+        http_addr = "127.0.0.1";
+        http_port = 2342;
+      };
+      analytics.reporting_enabled = false;
+    };
+
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://127.0.0.1:${toString config.services.prometheus.port}";
+        }
+        # {
+        #   name = "Loki";
+        #   type = "loki";
+        #   access = "proxy";
+        #   url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+        # }
+      ];
+    };
+  };
+
+  services.nginx.virtualHosts.${config.services.grafana.settings.server.domain} = {
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+      proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
       proxyWebsockets = true;
     };
     addSSL = true;
-    useACMEHost = ${config.services.grafana.domain};
+    useACMEHost = "crunch.poole.foo";
   };
+
 }
