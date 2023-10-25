45837875007ca1b33e5d617982fd05acc3ed1b51
David Poole <dtpoole@users.noreply.github.com>
2023-10-25 15:22:04 -0400

    fixes

--- hosts/crunch/postgres.nix
+++ hosts/crunch/postgres.nix
@@ -5,14 +5,14 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_15;
-    authentication = ''
+    authentication = lib.mkOverride ''
       # TYPE  DATABASE        USER            ADDRESS                 METHOD
       local   all             all                                     trust
       host    all             all             127.0.0.1/32            trust
       host    all             all             ::1/128                 trust
     '';
     settings = {
-      password_encryption = scram-sha-256;
+      password_encryption = "scram-sha-256";
     };
   };
 
