8914b0bd249d01284e22b4f6f30949d8f77df479
David Poole <dtpoole@users.noreply.github.com>
2023-10-25 15:16:39 -0400

    password_encryption

--- hosts/crunch/postgres.nix
+++ hosts/crunch/postgres.nix
@@ -11,6 +11,9 @@
       host    all             all             127.0.0.1/32            trust
       host    all             all             ::1/128                 trust
     '';
+    settings = {
+      password_encryption = scram-sha-256;
+    };
   };
 
   services.postgresqlBackup = {
