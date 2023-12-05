9e130626cd0118f75fdb74ed72c02a2bda44f706
David Poole <dtpoole@users.noreply.github.com>
2023-12-05 15:00:57 -0500

    Nix 23.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Make dream";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
