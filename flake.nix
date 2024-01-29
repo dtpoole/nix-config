d89db337aa647bffef78d06309b2941571fb8a59
David Poole <dtpoole@users.noreply.github.com>
2024-01-29 14:18:30 -0500

    Add soma

--- flake.nix
+++ flake.nix
@@ -78,6 +78,12 @@
           pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
         };
+
+        "dave@soma" = lib.homeManagerConfiguration {
+          modules = [ ./home/dave ];
+          pkgs = pkgsFor.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+        };
       };
 
     };
