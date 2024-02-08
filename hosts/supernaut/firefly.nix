27a7480f5bcf3e0b98b0e5b818bd23732d8c240a
David Poole <dtpoole@users.noreply.github.com>
2024-02-08 15:40:22 -0500

    Start to add Firefly III

---
+++ hosts/supernaut/firefly.nix
@@ -0,0 +1,38 @@
+{
+
+  services.postgresql = {
+    ensureDatabases = [ "firefly" ];
+    ensureUsers = [{
+      name = "firefly";
+      ensureDBOwnership = true;
+    }];
+  };
+
+  systemd.services."${config.virtualisation.oci-containers.backend}-firefly" = {
+    after = [ "postgresql.service" ];
+    requires = [ "postgresql.service" ];
+  };
+
+  virtualisation.oci-containers.containers = {
+    "firefly" = {
+      autoStart = true;
+      image = "fireflyiii/core:latest";
+      extraOptions = [ "--pull=always" ];
+      environment = {
+        "TZ" = "America/New_York";
+        "DB_CONNECTION" = "pgsql";
+        "DB_DATABASE" = "firefly";
+        "DB_USERNAME" = "firefly";
+        "DB_HOST" = "/run/postgresql/";
+      };
+      ports = [ "8080:8080" ];
+      volumes = [
+        "firefly:/var/www/html/storage/upload"
+        "/run/postgresql/:/run/postgresql/"
+      ];
+    };
+
+  };
+
+
+}
