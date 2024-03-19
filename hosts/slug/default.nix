6ac4cd691fb84c0d3becbfc54c05c1ae4d11cbbf
David Poole <dtpoole@users.noreply.github.com>
2024-03-19 16:29:46 -0400

    Fix slug

--- hosts/slug/default.nix
+++ hosts/slug/default.nix
@@ -1,12 +1,26 @@
-{ pkgs, ... }:
+{ inputs, outputs, ... }:
 
 {
+
+  networking.hostName = "slug";
+  nixpkgs.hostPlatform = "x86_64-linux";
+
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../modulez/desktop.nix
-      ../../modulez/zram.nix
+      ../common
+      ../common/desktop.nix
+
+      inputs.agenix.nixosModules.default
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.dave = import ../../home/dave/vm1.nix;
+          extraSpecialArgs = { inherit outputs; };
+        };
+      }
     ];
 
   # Bootloader.
@@ -15,77 +29,12 @@
 
   #boot.kernel.sysctl = { "vm.swappiness" = 25; };
 
-
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable sound with pipewire.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = false;
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   # If you want to use JACK applications, uncomment this
-  #   #jack.enable = true;
-
-  #   # use the example session manager (no others are packaged yet so this is enabled by default,
-  #   # no need to redefine it in your config for now)
-  #   #media-session.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    curl
-    vim
-    librewolf
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
   services.xrdp.enable = true;
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   hardware.cpu.intel.updateMicrocode = true;
 
