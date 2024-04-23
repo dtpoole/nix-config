a18b153bca4ccd7b0677c69b299e690625a5d6bf
David Poole <dtpoole@users.noreply.github.com>
2024-04-23 17:13:43 -0400

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
     mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6M0/XCWHHLcCWzwvao+COZ5hDb9/gQp7Yp6jZRcCdu";
     crunch = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG0mzjqkPiIaqwvnPbmnxWI3rXkS8141yP5eBUk8rhE";
     slug = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYeKyjR9GlfevtfOIqox7zAwla5y1cONc3lIkXcLD+g";
-    supernaut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjBbotEML8QUblJNj3YorpWcKy4B8UkD07yEQBm2Bf/";
+    supernaut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICngfzwaBuOLv8QUeK7vvTbffdSQlLoOVEzyo7kHM9d0";
     vm1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMe0y88ln1b5palqJLalarm+KeQ9Y18ErYcXoGhmnopo";
     bombs = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFcNrOGJNj5PrQgBmOvA3hef15/ZKmKrik5keay2mg46";
   };
