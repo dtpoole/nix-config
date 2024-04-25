07b5d2f30ff58467990ceb7291846292af860dba
David Poole <dtpoole@users.noreply.github.com>
2024-04-25 10:39:02 -0400

    listen on all interfaces

--- hosts/supernaut/vaultwarden.nix
+++ hosts/supernaut/vaultwarden.nix
@@ -6,6 +6,7 @@
     config = {
       DOMAIN = "https://vault.poole.fun";
       SIGNUPS_ALLOWED = true;
+      ROCKET_ADDRESS = "0.0.0.0";
       ROCKET_PORT = 8222;
       ADMIN_TOKEN = "$(cat ${config.age.secrets.vaultwarden_admin_token.path})";
     };
