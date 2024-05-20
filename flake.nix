21517080bc3b90187a1a84ebcfb2a329392cc4af
David Poole <dtpoole@users.noreply.github.com>
2024-05-20 09:51:47 -0400

    add sparkles

--- flake.nix
+++ flake.nix
@@ -109,6 +109,11 @@
           pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
         };
+        "dave@sparkles" = lib.homeManagerConfiguration {
+          modules = [ ./home/dave ];
+          pkgs = pkgsFor.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+        };
       };
 
     };
