6f4934dfe3b6ad4d9cd5a4d9aeaafa52a4659f45
David Poole <dtpoole@users.noreply.github.com>
2024-12-14 16:43:27 -0500

    add postgres config

--- hosts/sparkles/miniflux.nix
+++ hosts/sparkles/miniflux.nix
@@ -1,11 +1,23 @@
-{config, ...}:
-{
+{config, ...}: {
+  services.postgresql = {
+    ensureDatabases = ["miniflux"];
+    ensureUsers = [
+      {
+        name = "miniflux";
+        ensureDBOwnership = true;
+      }
+    ];
+  };
 
   age.secrets.miniflux_admin.file = ../../secrets/miniflux_admin.age;
 
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.age.secrets.miniflux_admin.path;
+    config = {
+      DATABASE_URL = "postgres://miniflux:@/miniflux?host=/run/postgresql";
+    };
   };
 
+  systemd.services.miniflux.after = ["postgresql.service"];
 }
