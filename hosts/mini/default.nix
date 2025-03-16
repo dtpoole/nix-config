d6f1549bcbd2e4226b5fd5f64b10c9ee8a8cbfb7
David Poole <dtpoole@users.noreply.github.com>
2025-03-16 16:27:46 -0400

    use new darwin modules

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -4,58 +4,45 @@
   ...
 }: let
   unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
-  username = "dave";
 in {
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  system.stateVersion = 5;
-
   imports = [
-    inputs.agenix.darwinModules.default
-    inputs.home-manager.darwinModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.${username} = import ../../modules/home-manager/desktop.nix;
-        extraSpecialArgs = {inherit username;};
-      };
-    }
-    ./homebrew.nix
-    ./mac.nix
+    ../../modules/darwin
   ];
 
-  users.users.${username}.home = "/Users/${username}";
+  # Mini-specific configuration
+  darwin.homebrew = {
+    extraBrews = [
+      "colima"
+      "docker"
+      "ffmpeg"
+      "flac"
+      "libdvdcss"
+    ];
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
+    extraCasks = [
+      "balenaetcher"
+      "calibre"
+      "fission"
+      "gzdoom"
+      "handbrake"
+      "ioquake3"
+      "logi-options+"
+      "makemkv"
+      "musicbrainz-picard"
+      "mullvad-browser"
+      "retroarch-metal"
+      "vmware-fusion"
+      "xld"
+    ];
+
+    extraMasApps = {
+      "The Unarchiver" = 425424353;
+      "Windows App" = 1295203466;
     };
   };
 
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      warn-dirty = false;
-    };
-  };
-
-  # Make sure the nix daemon always runs
-  services.nix-daemon.enable = true;
-
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  #services.nix-daemon.package = pkgs.nixFlakes;
-
-  # if you use zsh (the default on new macOS installations),
-  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
-  programs.zsh.enable = true;
-
-  environment.shells = [pkgs.zsh];
-  environment.systemPackages = [
-    unstablePkgs.yt-dlp
-    inputs.agenix.packages.${pkgs.system}.default
-    pkgs.nixd
+  # Add any Mini-specific packages
+  environment.systemPackages = with unstablePkgs; [
+    yt-dlp
   ];
-
-  # for nixd
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 }
