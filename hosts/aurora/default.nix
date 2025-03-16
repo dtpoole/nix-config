f08ca58bf780c43c31f5c3b1885e114829457430
David Poole <dtpoole@users.noreply.github.com>
2025-03-16 16:33:53 -0400

    use new darwin modules

--- hosts/aurora/default.nix
+++ hosts/aurora/default.nix
@@ -4,58 +4,18 @@
   ...
 }: let
   unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
-  username = "dave";
 in {
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  system.stateVersion = 5;
-
   imports = [
-    inputs.agenix.darwinModules.default
-    inputs.home-manager.darwinModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.${username} = import ../../modules/home-manager/desktop.nix;
-        extraSpecialArgs = {inherit username;};
-      };
-    }
-    ./homebrew.nix
-    ./mac.nix
+    ../../modules/darwin
   ];
 
-  users.users.${username}.home = "/Users/${username}";
-
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
+  # Aurora-specific configuration can be added here
+  darwin.homebrew = {
+    # Any Aurora-specific brews, casks, or Mac App Store apps would go here
   };
 
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      warn-dirty = false;
-    };
-  };
-
-  # Make sure the nix daemon always runs
-  services.nix-daemon.enable = true;
-
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  #services.nix-daemon.package = pkgs.nixFlakes;
-
-  # if you use zsh (the default on new macOS installations),
-  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
-  programs.zsh.enable = true;
-
-  environment.shells = [pkgs.zsh];
-  environment.systemPackages = [
-    unstablePkgs.yt-dlp
-    inputs.agenix.packages.${pkgs.system}.default
-    pkgs.nixd
+  # Add any Aurora-specific packages
+  environment.systemPackages = with unstablePkgs; [
+    yt-dlp
   ];
-
-  # for nixd
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 }
