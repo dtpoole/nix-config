18ca9315372d769836a7bf7d6c4457587caa78f1
David Poole <dtpoole@users.noreply.github.com>
2025-05-12 20:44:20 -0400

    Remove eplustv

--- hosts/sapphire/containers.nix
+++ hosts/sapphire/containers.nix
@@ -1,5 +1,4 @@
 {config, ...}: {
-
   age.secrets.mlbserver_env.file = ../../secrets/mlbserver_env.age;
 
   virtualisation.podman.autoPrune = {
@@ -10,19 +9,6 @@
   };
 
   virtualisation.oci-containers.containers = {
-    "eplustv" = {
-      autoStart = true;
-      image = "m0ngr31/eplustv";
-      extraOptions = ["--pull=always"];
-      environment = {
-        "PORT" = "8000";
-      };
-      ports = ["100.91.24.97:8000:8000"];
-      volumes = [
-        "eplustv:/app/config"
-      ];
-    };
-
     "mlbserver" = {
       autoStart = true;
       image = "tonywagner/mlbserver";
