d7645f3c088d150424f7414196d48d691003dd65
David Poole <dtpoole@users.noreply.github.com>
2025-05-20 17:17:01 -0400

    add multiplexing config. update host ips

--- modules/home-manager/ssh-config.nix
+++ modules/home-manager/ssh-config.nix
@@ -11,38 +11,44 @@
     programs.ssh = {
       enable = true;
       extraConfig = ''
-        StrictHostKeyChecking no
+        # trusted network
+        Host 10.10.2.*
+          StrictHostKeyChecking accept-new
       '';
+      controlMaster = "auto";
+      controlPersist = "4h";
+      hashKnownHosts = true;
+
       matchBlocks = {
         "tank" = {
-          hostname = "10.10.10.40";
+          hostname = "10.10.2.40";
           user = "tank";
         };
         "mini" = {
-          hostname = "10.10.10.60";
+          hostname = "10.10.2.60";
         };
         "jellyfin" = {
-          hostname = "10.10.10.35";
+          hostname = "10.10.2.35";
           user = "root";
         };
         "plex" = {
-          hostname = "10.10.10.36";
+          hostname = "10.10.2.36";
           user = "root";
         };
         "slurp" = {
-          hostname = "10.10.10.37";
+          hostname = "10.10.2.37";
           user = "media";
         };
         "caddy" = {
-          hostname = "10.10.10.39";
+          hostname = "10.10.2.39";
           user = "root";
         };
         "minecraft" = {
-          hostname = "10.10.10.145";
+          hostname = "10.10.2.145";
           user = "root";
         };
         "minecraft-java" = {
-          hostname = "10.10.10.146";
+          hostname = "10.10.2.146";
           user = "root";
         };
       };
