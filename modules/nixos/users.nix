58c78c05fe4e59359862d5a4f00e4cba64345aa1
David Poole <dtpoole@users.noreply.github.com>
2025-05-23 10:32:16 -0400

    cleanup authorized keys

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -20,9 +20,7 @@
       hashedPasswordFile = config.age.secrets.user_password.path;
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ dave@mini"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8HIreGLHjE8f2kpfd49WRCfXXk2oMRApuIW78BWYVi dave@air"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqh47ik6EctDQLPpEClM3LTqBSt3CbonGSUlW7DCc+/"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8vKcWXqq+nA88NE2/mfCpu1bR1w8OJtJRTfTVqbbLR"
       ];
     };
