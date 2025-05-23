a42461af620e13eb03e0c6fc9e57b3b9f774ac23
David Poole <dtpoole@users.noreply.github.com>
2025-05-23 13:58:01 -0400

    Lint: remove repeated-keys

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -9,122 +9,123 @@
   };
 
   config = lib.mkIf config.programs.enable {
-    programs.bat = {
-      enable = true;
-      config = {
-        italic-text = "always";
-        tabs = "2";
-        theme = "Nord";
-      };
-    };
-
-    programs.btop = {
-      enable = true;
-      settings = {
-        color_theme = "nord";
-        theme_background = false;
-      };
-    };
-
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-      config = {
-        hide_env_diff = true;
-        DIRENV_WARN_TIMEOUT = 0;
-      };
-    };
-
-    programs.htop = {
-      enable = true;
-      settings = {
-        hide_kernel_threads = true;
-        hide_threads = true;
-        hide_userland_threads = true;
-        show_program_path = false;
-        tree_view = false;
-        vim_mode = true;
-      };
-    };
-
-    programs.git = {
-      enable = true;
-      userName = "David Poole";
-      userEmail = "dtpoole@users.noreply.github.com";
-      aliases = {
-        st = "status";
-        co = "checkout";
-        br = "branch";
-        up = "rebase";
-        ci = "commit";
-      };
-      delta = {
+    programs = {
+      bat = {
         enable = true;
-        options = {
-          side-by-side = "true";
+        config = {
+          italic-text = "always";
+          tabs = "2";
           theme = "Nord";
         };
       };
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = "true";
-      };
-      ignores = [
-        "*~"
-        ".DS_Store"
-        ".vscode"
-        "Thumbs.db"
-      ];
-    };
 
-    programs.tmux = {
-      enable = true;
-      historyLimit = 10000;
-      plugins = with pkgs.tmuxPlugins; [
-        nord
-      ];
-      extraConfig = ''
-        setw -g mouse on
-        set -g prefix C-a
-        unbind C-b
-
-        bind a send-prefix
-        bind-key C-a last-window
-        bind-key S command-prompt -p ssh: "new-window -n %1 'ssh %1'"
-      '';
-    };
-
-    programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        command_timeout = 1000;
-        container = {
-          disabled = true;
-        };
-        username = {
-          style_user = "bold green";
-          format = "[$user]($style)";
-        };
-        hostname = {
-          ssh_symbol = "@";
-          style = "bold green";
-          format = "[$ssh_symbol$hostname]($style) ";
-        };
-        shlvl = {
-          disabled = false;
-          format = "$shlvl ▼ ";
-          threshold = 4;
+      btop = {
+        enable = true;
+        settings = {
+          color_theme = "nord";
+          theme_background = false;
         };
       };
-    };
 
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+        config = {
+          hide_env_diff = true;
+          DIRENV_WARN_TIMEOUT = 0;
+        };
+      };
 
-    programs.zoxide = {
+      htop = {
+        enable = true;
+        settings = {
+          hide_kernel_threads = true;
+          hide_threads = true;
+          hide_userland_threads = true;
+          show_program_path = false;
+          tree_view = false;
+          vim_mode = true;
+        };
+      };
+
+      git = {
+        enable = true;
+        userName = "David Poole";
+        userEmail = "dtpoole@users.noreply.github.com";
+        aliases = {
+          st = "status";
+          co = "checkout";
+          br = "branch";
+          up = "rebase";
+          ci = "commit";
+        };
+        delta = {
+          enable = true;
+          options = {
+            side-by-side = "true";
+            theme = "Nord";
+          };
+        };
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.rebase = "true";
+        };
+        ignores = [
+          "*~"
+          ".DS_Store"
+          ".vscode"
+          "Thumbs.db"
+        ];
+      };
+
+      tmux = {
+        enable = true;
+        historyLimit = 10000;
+        plugins = with pkgs.tmuxPlugins; [
+          nord
+        ];
+        extraConfig = ''
+          setw -g mouse on
+          set -g prefix C-a
+          unbind C-b
+
+          bind a send-prefix
+          bind-key C-a last-window
+          bind-key S command-prompt -p ssh: "new-window -n %1 'ssh %1'"
+        '';
+      };
+
+      starship = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          command_timeout = 1000;
+          container = {
+            disabled = true;
+          };
+          username = {
+            style_user = "bold green";
+            format = "[$user]($style)";
+          };
+          hostname = {
+            ssh_symbol = "@";
+            style = "bold green";
+            format = "[$ssh_symbol$hostname]($style) ";
+          };
+          shlvl = {
+            disabled = false;
+            format = "$shlvl ▼ ";
+            threshold = 4;
+          };
+        };
+      };
+
+      zoxide = {
         enable = true;
         options = [
-        "--cmd cd"
+          "--cmd cd"
         ];
+      };
     };
   };
 }
