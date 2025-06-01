b76b210ae4d457fc7e76b2496bdc0a6deb6e7644
David Poole <dtpoole@users.noreply.github.com>
2025-05-31 21:55:27 -0400

    use hashedPassword

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -9,20 +9,22 @@
   };
 
   config = lib.mkIf config.users.enable {
-    age.secrets.user_password.file = ../../secrets/user_password.age;
-
-    users.users.dave = {
-      isNormalUser = true;
-      home = "/home/dave";
-      group = "users";
-      description = lib.mkForce "David Poole";
-      extraGroups = ["docker" "wheel" "networkmanager"];
-      hashedPasswordFile = config.age.secrets.user_password.path;
-      shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8vKcWXqq+nA88NE2/mfCpu1bR1w8OJtJRTfTVqbbLR"
-      ];
+    users = {
+      mutableUsers = false;
+      users.dave = {
+        isNormalUser = true;
+        home = "/home/dave";
+        group = "users";
+        description = lib.mkForce "David Poole";
+        extraGroups = ["docker" "wheel" "networkmanager"];
+        hashedPassword = "$6$faPngAjiSid77.8t$8iLvE/E.ChK2GD.52gp4oMqG7gY.Gyd2TkfV.solZF9SMKSTmjGhkN.nJDwS26QLjAo3aAf4PCbEqrWJN2/cZ/";
+        shell = pkgs.zsh;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC8vKcWXqq+nA88NE2/mfCpu1bR1w8OJtJRTfTVqbbLR"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcId6n5aN76tFqvdA42ECtsICDyh/1l+pC841ZBgENL"
+        ];
+      };
     };
 
     programs.zsh.enable = true;
