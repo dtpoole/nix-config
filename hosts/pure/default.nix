cc7ae9d850597f4bff77efcf1c9eafd5a9ce4b9c
David Poole <dtpoole@users.noreply.github.com>
2025-05-31 20:39:43 -0400

    use static ip

--- hosts/pure/default.nix
+++ hosts/pure/default.nix
@@ -1,7 +1,6 @@
 {inputs, ...}: let
   username = "dave";
 in {
-
   nixpkgs.hostPlatform = "x86_64-linux";
 
   imports = [
@@ -23,26 +22,37 @@ in {
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Optimize for virtualized environment
-  boot.kernelParams = [ "elevator=noop" ];
+  boot.kernelParams = ["elevator=noop"];
 
   networking = {
     hostName = "pure";
-    dhcpcd.enable = true;
+    dhcpcd.enable = false;
+    interfaces.ens18 = {
+      ipv4.addresses = [
+        {
+          address = "10.10.2.45";
+          prefixLength = 24;
+        }
+      ];
+    };
+    defaultGateway = "10.10.2.1";
 
     firewall.enable = true;
     nftables.enable = true;
   };
 
-  services.openssh.openFirewall = false;
+  services.lldpd.enable = true;
+
+  services.openssh.openFirewall = true;
 
   services.beszel-agent.enable = true;
   services.qemuGuest.enable = true;
 
-  dns.enable = true;
+  dns.enable = false;
   healthchecks-ping.enable = false;
   netdata.enable = false;
   postgres.enable = false;
   restic.enable = false;
-  tailscale.enable = true;
+  tailscale.enable = false;
   zram.enable = true;
 }
