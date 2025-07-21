dcff72ab52a65bf8c6973330c849159bb737f73e
David Poole <dtpoole@users.noreply.github.com>
2025-07-21 10:21:53 -0400

    secret rekey and cleanup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,11 @@
 let
   systems = {
     mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6M0/XCWHHLcCWzwvao+COZ5hDb9/gQp7Yp6jZRcCdu";
-    slug = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYeKyjR9GlfevtfOIqox7zAwla5y1cONc3lIkXcLD+g";
-    supernaut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICngfzwaBuOLv8QUeK7vvTbffdSQlLoOVEzyo7kHM9d0";
     sparkles = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItEj1cbmZqIyGZgLfwIb3jmr7byFfTWrMf4FevPsxzn";
     jumbo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+jRGsr5gDvuAZVInvp6IuLeV7lRD5u8GTbGNmDRa5j";
     vm1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlsETz033DfRhx3oWV8smITgaEh2wf5euhlVyPNUd0W";
     sapphire = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyWx8Tkh7ORKbXPZ5oclwOxPdmAG39zujS0f7n4unQa";
+    pure = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0hFkXxBFuXWhP8A7mI2ReXVCIjzdcBjLAnwYdNRkeg";
   };
   users = {
     dave_mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDL8vV4xFbHiAkqYOSgwT2hdTVtnXqH5yC2mZEsQUnuJ";
@@ -16,12 +15,9 @@ let
   allSystems = builtins.attrValues systems;
 in {
   "user_password.age".publicKeys = allUsers ++ allSystems;
-  "acme_cloudflare_credentials.age".publicKeys = allUsers ++ [systems.supernaut systems.sparkles];
-  "supernaut_hc_ping_uuid.age".publicKeys = allUsers ++ [systems.supernaut];
-  "supernaut_hc_restic_uuid".publicKeys = allUsers ++ [systems.supernaut];
-  "supernaut_vaultwarden_admin_token.age".publicKeys = allUsers ++ [systems.supernaut];
-  "linkding_password.age".publicKeys = allUsers ++ [systems.sparkles];
+  "acme_cloudflare_credentials.age".publicKeys = allUsers ++ [systems.pure systems.sparkles];
   "tailscale_auth_key.age".publicKeys = allUsers ++ allSystems;
+
   "jumbo_hc_ping_uuid.age".publicKeys = allUsers ++ [systems.jumbo];
   "sapphire_hc_ping_uuid.age".publicKeys = allUsers ++ [systems.sapphire];
   "sparkles_hc_ping_uuid.age".publicKeys = allUsers ++ [systems.sparkles];
@@ -30,13 +26,12 @@ in {
   "restic/cloud/env.age".publicKeys = allUsers ++ [systems.sparkles];
   "restic/cloud/repo.age".publicKeys = allUsers ++ [systems.sparkles];
   "restic/cloud/password.age".publicKeys = allUsers ++ [systems.sparkles];
+  "restic/local/repo.age".publicKeys = allUsers;
+  "restic/local/password.age".publicKeys = allUsers;
 
-  "restic/local/repo.age".publicKeys = allUsers ++ [systems.supernaut];
-  "restic/local/password.age".publicKeys = allUsers ++ [systems.supernaut];
-
-  "eplustv_env.age".publicKeys = allUsers ++ [systems.jumbo];
+  "linkding_password.age".publicKeys = allUsers ++ [systems.sparkles];
   "searxng_secret.age".publicKeys = allUsers ++ [systems.sparkles];
   "miniflux_admin.age".publicKeys = allUsers ++ [systems.sparkles];
-
   "mlbserver_env.age".publicKeys = allUsers ++ [systems.sapphire];
+  "vaultwarden_admin_token.age".publicKeys = allUsers ++ [systems.pure];
 }
