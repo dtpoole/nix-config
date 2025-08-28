69c6217758a9f9604fce61f893c0c30b419970c6
David Poole <dtpoole@users.noreply.github.com>
2025-08-28 11:27:02 -0400

    make simpler

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,3 @@
-{inputs}: final: prev: let
-  unstablePkgs = [
-    "tailscale"
-    "yt-dlp"
-    "beszel"
-  ];
-in {
-  unstable = prev.lib.genAttrs unstablePkgs (
-    name:
-      inputs.nixpkgs-unstable.legacyPackages.${final.system}.${name}
-  );
+{inputs}: final: prev: {
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${final.system};
 }
