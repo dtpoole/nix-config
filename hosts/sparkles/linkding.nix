375cfcec81ea55c5635999cf7730194b42f620ca
David Poole <dtpoole@users.noreply.github.com>
2025-09-12 19:53:00 -0400

    version bump

--- hosts/sparkles/linkding.nix
+++ hosts/sparkles/linkding.nix
@@ -1,5 +1,5 @@
 {config, ...}: let
-  version = "1.40.0-plus";
+  version = "1.42.0-plus";
 in {
   systemd.services."${config.virtualisation.oci-containers.backend}-linkding" = {
     after = ["postgresql.service"];
