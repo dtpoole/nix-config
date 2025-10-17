bdc133d9a5b67ba16b9678e27b859d0faeb8b24c
David Poole <dtpoole@users.noreply.github.com>
2025-10-17 18:20:57 -0400

    add modern algos/ciphers

--- modules/nixos/sshd.nix
+++ modules/nixos/sshd.nix
@@ -17,7 +17,33 @@
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
+
+        KexAlgorithms = [
+          "curve25519-sha256"
+          "curve25519-sha256@libssh.org"
+          "diffie-hellman-group-exchange-sha256"
+        ];
+        Ciphers = [
+          "chacha20-poly1305@openssh.com"
+          "aes256-gcm@openssh.com"
+          "aes128-gcm@openssh.com"
+          "aes256-ctr"
+          "aes192-ctr"
+          "aes128-ctr"
+        ];
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
+
+        MaxAuthTries = 3;
+        MaxSessions = 10;
+        ClientAliveInterval = 300;
+        ClientAliveCountMax = 2;
+        LoginGraceTime = 60;
       };
+
       extraConfig = ''
         AllowTcpForwarding yes
         AllowAgentForwarding no
