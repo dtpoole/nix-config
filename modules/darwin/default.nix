6578d7717765454a74cc1b6979ee1d7a62b3a3d0
David Poole <dtpoole@users.noreply.github.com>
2025-10-17 21:06:50 -0400

    additional nix settings

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -23,9 +23,37 @@
   };
 
   nix = {
+    gc = {
+      automatic = true;
+      interval = [
+        {
+          Hour = 14;
+          Minute = 15;
+          Weekday = 4;
+        }
+      ];
+      options = "--delete-older-than 1w";
+    };
+    optimise.automatic = true;
     settings = {
       experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
+      download-buffer-size = 268435456;
+      http-connections = 25;
+      connect-timeout = 5;
+      max-jobs = "auto";
+      cores = 0;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+  
+      min-free = 1 * 1024 * 1024 * 1024;
+      max-free = 25 * 1024 * 1024 * 1024;
     };
   };
 
