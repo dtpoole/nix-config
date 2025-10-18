929effbb5e09a5f784b7e5a2961348c46b01d3d1
David Poole <dtpoole@users.noreply.github.com>
2025-10-17 21:36:35 -0400

    add container resource limits

--- hosts/sapphire/containers.nix
+++ hosts/sapphire/containers.nix
@@ -13,7 +13,13 @@
     "mlbserver" = {
       autoStart = true;
       image = "tonywagner/mlbserver";
-      extraOptions = ["--pull=always"];
+      extraOptions = [
+        "--pull=always"
+        "--memory=1g"
+        "--memory-swap=2g"
+        "--cpus=2.0"
+        "--pids-limit=200"
+      ];
       environmentFiles = [config.age.secrets.mlbserver_env.path];
       ports = ["100.91.24.97:9999:9999"];
       volumes = [
