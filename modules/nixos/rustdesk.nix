173a942b180742ddc5e9f0bbf83770122d959f85
David Poole <dtpoole@users.noreply.github.com>
2026-01-05 16:26:08 -0500

    rustdesk

--- modules/nixos/rustdesk.nix
+++ modules/nixos/rustdesk.nix
@@ -40,6 +40,8 @@
       relay-server = '${config.rustdesk.serverAddress}'
       api-server = '${config.rustdesk.serverAddress}'
       key = 'w0azzaRTrIJU08jghF5EJCLFF0HxgUlHQwt9BhTk3Pw='
+      direct-server = 'Y'
+      direct-access-port = '21118'
       EOF
       chown -R dave:users /home/dave/.config/rustdesk
     '';
